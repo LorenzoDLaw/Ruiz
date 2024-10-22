929512738768513657820145b887802b0ec53dde
LorenzoSca <149068830+LorenzoDLaw@users.noreply.github.com>
2024-10-22 09:58:33 +0200

    22/10 II

--- ScanuLorenzo/Calcolatrice/src/calcolatrice/Calcolatrice.java
+++ ScanuLorenzo/Calcolatrice/src/calcolatrice/Calcolatrice.java
@@ -11,26 +11,30 @@ import java.util.Scanner;
  * @author alunni
  */
 public class Calcolatrice {
-    int n1;
-    int n2;
+    static double n1;
+    static double n2;
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Inserisci l'operatore tra +, -, /, *:");
         char operatore = scanner.next().charAt(0);
         System.out.println("Inserisci il primo numero:");
+        n1 = scanner.nextDouble();
         System.out.println("Inserisci il secondo numero");
-        
+        n2 = scanner.nextDouble();
+        Operation operazione = new Operation(n1, n2);
         switch (operatore) {
             case '+':
-                
+                System.out.println( operazione.addizione());
                 break;
             case '-':
-                
+                System.out.println( operazione.sottrazione());
                 break;
             case '/':
+                System.out.println( operazione.divisione());
                 break;
             case '*':
+                System.out.println( operazione.prodotto());
         }
     }
     
