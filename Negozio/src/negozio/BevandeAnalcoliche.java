76d20948d075ef648f4e491f5a8f7289972c5706
LorenzoSca <149068830+LorenzoDLaw@users.noreply.github.com>
2024-12-10 08:52:38 +0100

    Inizio progetto negozio
    Ereditarietà delle classe prodotto
    -Primo ramo bevande
    -Finire bevande analcoliche 
    -Testare il funzionamento

---
+++ Negozio/src/negozio/BevandeAnalcoliche.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package negozio;
+
+/**
+ *
+ * @author alunni
+ */
+public class BevandeAnalcoliche extends Bevande{
+    
+    public BevandeAnalcoliche(String nome, double prezzo, int id, int quantitaInCl) {
+        super(nome, prezzo, id, quantitaInCl);
+    }
+    
+    
+}
