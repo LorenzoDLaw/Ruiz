0546ad33ad5c6435f075504505ff46eecd4843d6
Lorenzo <149068830+LorenzoDLaw@users.noreply.github.com>
2025-02-15 11:34:18 +0100

    riorganizzazione

--- Lorenzo_Scanu/Libreria/src/libreria/Biblioteca.java
+++ Lorenzo_Scanu/Libreria/src/libreria/Biblioteca.java
@@ -1,19 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package libreria;
 import java.util.ArrayList;
 import java.util.Scanner;
-/**
- *
- * @author alunni
- */
+
 public class Biblioteca {
     ArrayList<Libro> biblioteca = new ArrayList<>();
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
         while(true){
@@ -28,7 +19,8 @@ public class Biblioteca {
                 switch (c) {
                     case 1:
                            System.out.println("Aggiungere libro");
-                           
+                           Libro lib = aggiungiLibro();
+                           biblioteca.add(lib);
                         break;
                     case 2:
                         System.out.println("Modifica disponibilità");
@@ -51,7 +43,7 @@ public class Biblioteca {
     } // fine main
     
     
-    public void aggiungiLibro(){
+    public static Libro aggiungiLibro(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Titolo del libro: ");
         String titolo = scanner.nextLine();
@@ -61,5 +53,8 @@ public class Biblioteca {
         String genere = scanner.next();
         System.out.println("Libro in prestito (true/false): ");
         boolean disponibile = scanner.nextBoolean();
+        
+        Libro libro = new Libro(titolo, autore, genere, disponibile);
+        return libro;
     }
 }
